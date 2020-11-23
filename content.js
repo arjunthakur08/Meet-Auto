0492977b6429ce69f465bf49564a4faa4b2dabbb
arjunthakur08 <arjunthakur01081997@gmail.com>
2020-11-24 02:49:05 +0530

    v0.0.2

--- content.js
+++ content.js
@@ -1,87 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
 	'use strict';
-
 	
-
-	// div with "contain-402" changes when someone try to join the meet.
-	// Only observe that div, if it is available
-	function addObserverIfDesiredNodeAvailable() 
-	{
-		let container = document.getElementById('contain-402');	
-		
-		// Check if observer should start or not
-		if(!container) {
-			//The node we need does not exist yet.
-			//Wait 500ms and try again
-			window.setTimeout(addObserverIfDesiredNodeAvailable,500);
-			// console.log("Node not found!")
-			return;
-		}
-		
-		let options = {
-				attributes: true,
-				attributeFilter: ['aria-hidden']
-			},
-			observer = new MutationObserver(myCallback);
-		
-		// Callback function for MutationObserver
-		function myCallback (mutations) 
-		{
-			for (let mutation of mutations) 
-			{
-				if (mutation.type === 'attributes' && mutation.attributeName === 'aria-hidden') 
-					if(container.hasAttribute('aria-hidden'))
-						setTimeout(autoAdmit, 500);		// Calling the autoAdmit Function
-			}
-		}
-
-		// Telling the MutationObserver to observe
-		observer.observe(container, options);
-	}
-
-	//setInterval(autoAdmit, 100);	
 
 	// Function performing auto-admitting students to the MEET whomever has the link 
 	function autoAdmit()
 	{
-		// Finding the element which contains the string = "Admit"
-		let p = [...document.querySelectorAll("*")].filter(e => e.childNodes && [...e.childNodes].find(n => n.nodeValue?.match("Admit")));
-		
-		// The code
-		if(p[0] !== undefined)
-		{	
-	
-			// Better approach just to grab one element using querySelector
-			let dialogBoxElement = document.querySelector('div[role="dialog"]');
-
-			let imageElements = dialogBoxElement.querySelectorAll('img');
-
-			let userImg, userName;
-
-			imageElements.forEach(img => {
-				if(img.src.indexOf("googleusercontent") > -1)
-				{
-					userImg = img
-				}
-			});
-
-			userName = userImg.title;
-	
-			let myNode = p[0];
-			while(myNode)
+		// Getting all buttons which are inside any dialog box
+		let btns = document.querySelectorAll('div[role="dialog"] div[role="button"]');		
+		btns.forEach(function(btn){
+			if(btn.innerText.toLowerCase()=='admit' || btn.innerText.toLowerCase()=="admit all")
+				btn.click();
+			else if (btn.innerText.toLowerCase()=='view all')
 			{
-				myNode = myNode.parentNode;
-				if(myNode.hasAttribute('role'))
-					if(myNode.getAttribute('role')=="button")    
-						break;
+				btn.click();
+				setTimeout(function(){btn.click();},800);
+				
 			}
-			// Finding actual element and getting the 'div'
-			let admitId = myNode.getAttribute('data-id');
-			document.querySelector(`div[data-id="${admitId}"]`).click();
-			console.log(userName,"Admitted Successfully @ ",new Date());
-		}
+		});
 	}
-
-	addObserverIfDesiredNodeAvailable();
-
+	
+	setInterval(autoAdmit, 500);	
 });
